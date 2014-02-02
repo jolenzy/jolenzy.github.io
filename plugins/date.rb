a7d555ac550d66332d851ade240a8ca27e9b3563
jolenzy <contact@jovicailic.org>
2014-02-02 13:21:31 +0000

    date format change

--- plugins/date.rb
+++ plugins/date.rb
@@ -1,6 +1,21 @@
+# encoding: utf-8
 module Octopress
   module Date
+    # Названия дней недели -- контекстные и отдельностоящие
+    COMMOM_DAYNAMES_RU = ["недеља", "понедељак", "уторак", "среда", "четвртак", "петак", "субота"]
+    DAYNAMES_RU = ["Недеља", "Понедељак", "Уторак", "Среда", "Четвртак", "Петак", "Субота"]
+    ABBR_DAYNAMES_RU = ["Недеља", "Понедељак", "Уторак", "Среда", "Четвртак", "Петак", "Субота"]
+    STANDALONE_ABBR_DAYNAMES_RU = ["нед", "пон", "уто", "сре", "чет", "пет", "суб"]
 
+    # Названия месяцев -- сокращенные и полные, плюс отдельностоящие.
+    # Не забудьте nil в начале массиве (~)
+    # 
+    #
+    # Don't forget the nil at the beginning; there's no such thing as a 0th month
+    COMMON_MONTHNAMES_RU = [nil, "јануар", "фебруар", "март", "април", "мај", "јун", "јул", "август", "септембар", "октобар", "новембар", "децембар"]
+    MONTHNAMES_RU = [nil, "Јануар", "Фебруар", "Март", "Април", "Мај", "Јун", "Јул", "Август", "Септембар", "Октобар", "Новембар", "Децембар"]
+    COMMON_ABBR_MONTHNAMES_RU = [nil, "јан.", "феб.", "мар.", "апр.", "мај", "јун", "јул", "авг.", "сеп.", "окт.", "нов.", "дец."]
+    ABBR_MONTHNAMES_RU = [nil, "Јан.", "Феб.", "Мар.", "Апр.", "Мај", "Јун", "Јул", "Авг.", "Сеп.", "Окт.", "Нов.", "Дец."]
     # Returns a datetime if the input is a string
     def datetime(date)
       if date.class == String
@@ -11,8 +26,10 @@ module Octopress
 
     # Returns an ordidinal date eg July 22 2007 -> July 22nd 2007
     def ordinalize(date)
-      date = datetime(date)
-      "#{date.strftime('%b')} #{ordinal(date.strftime('%e').to_i)}, #{date.strftime('%Y')}"
+      #date = datetime(date)
+      format_date(date, "%a, %e %B %Y")
+      #format_date(date, "%d %B %Y")
+     # "#{date.strftime('%b')} #{ordinal(date.strftime('%e').to_i)}, #{date.strftime('%Y')}"
     end
 
     # Returns an ordinal number. 13 -> 13th, 21 -> 21st etc.
@@ -36,19 +53,14 @@ module Octopress
       if format.nil? || format.empty? || format == "ordinal"
         date_formatted = ordinalize(date)
       else
+        format.gsub!(/%a/, ABBR_DAYNAMES_RU[date.wday])
+        format.gsub!(/%A/, DAYNAMES_RU[date.wday])
+        format.gsub!(/%b/, ABBR_MONTHNAMES_RU[date.mon])
+        format.gsub!(/%B/, COMMON_MONTHNAMES_RU[date.mon])
         date_formatted = date.strftime(format)
-        date_formatted.gsub!(/%o/, ordinal(date.strftime('%e').to_i))
+        #date_formatted.gsub!(/%o/, ordinal(date.strftime('%e').to_i))
       end
       date_formatted
-    end
-    
-    # Returns the date-specific liquid attributes
-    def liquid_date_attributes
-      date_format = self.site.config['date_format']
-      date_attributes = {}
-      date_attributes['date_formatted']    = format_date(self.data['date'], date_format)    if self.data.has_key?('date')
-      date_attributes['updated_formatted'] = format_date(self.data['updated'], date_format) if self.data.has_key?('updated')
-      date_attributes
     end
 
   end
@@ -60,22 +72,54 @@ module Jekyll
   class Post
     include Octopress::Date
 
-    # Convert this Convertible's data to a Hash suitable for use by Liquid.
-    # Overrides the default return data and adds any date-specific liquid attributes
-    alias :super_to_liquid :to_liquid
+    # Convert this post into a Hash for use in Liquid templates.
+    #
+    # Returns <Hash>
     def to_liquid
-      super_to_liquid.deep_merge(liquid_date_attributes)
+      date_format = self.site.config['date_format']
+      self.data.deep_merge({
+        "title"             => self.data['title'] || self.slug.split('-').select {|w| w.capitalize! || w }.join(' '),
+        "url"               => self.url,
+        "date"              => self.date,
+        # Monkey patch
+        "date_formatted"    => format_date(self.date, date_format),
+        "updated_formatted" => self.data.has_key?('updated') ? format_date(self.data['updated'], date_format) : nil,
+        "id"                => self.id,
+        "categories"        => self.categories,
+        "next"              => self.next,
+        "previous"          => self.previous,
+        "tags"              => self.tags,
+        "content"           => self.content })
     end
   end
 
   class Page
     include Octopress::Date
 
-    # Convert this Convertible's data to a Hash suitable for use by Liquid.
-    # Overrides the default return data and adds any date-specific liquid attributes
-    alias :super_to_liquid :to_liquid
-    def to_liquid
-      super_to_liquid.deep_merge(liquid_date_attributes)
+    # Initialize a new Page.
+    #
+    # site - The Site object.
+    # base - The String path to the source.
+    # dir  - The String path between the source and the file.
+    # name - The String filename of the file.
+    def initialize(site, base, dir, name)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = name
+
+      self.process(name)
+      self.read_yaml(File.join(base, dir), name)
+      # Monkey patch
+      date_format = self.site.config['date_format']
+      self.data['date_formatted']    = format_date(self.data['date'], date_format) if self.data.has_key?('date')
+      self.data['updated_formatted'] = format_date(self.data['updated'], date_format) if self.data.has_key?('updated')
+    end
+  end
+  module Filters
+    include Octopress::Date
+    def date_ru(date, format)
+      format_date(date, format)
     end
   end
 end
